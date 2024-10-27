79fcb7ef134fd31eeac760eb0689656d5ee322c8
Jean Moreira <jeancm110@gmail.com>
2024-10-27 18:06:54 -0300

    Busca oculta

---
+++ script.js
@@ -0,0 +1,10 @@
+//Adiciona um evento de click ao botão
+
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', () => {
+  search.classList.toggle('active');
+  input.focus();
+});
